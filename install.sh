dc67855b2d5028eb2d91b6b4b0305ac40079fb3f
Jesus Aguilera <jeaguil@github.com>
2024-12-02 14:09:20 -0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git config --global user.name "Jesus Aguilera"
+git config --global user.email "jeaguil@github.com"
+git config --global core.editor vim
+git config --global pager.branch false
+git config --global push.default current
