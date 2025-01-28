3ec7b2d62eb1a845ccfbfc65800d15be485ca82e
Jesus Aguilera <jeaguil@github.com>
2025-01-28 09:46:20 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,24 @@
 
 git config --global user.name "Jesus Aguilera"
 git config --global user.email "jeaguil@github.com"
+
 git config --global core.editor vim
+
 git config --global pager.branch false
+
 git config --global push.default current
+
+git config --global alias.l '!. ~/.dotfiles/.githelpers && pretty_git_log'
+git config --global alias.la '!git l --all'
+
+# git pull-remote <branch-name>
+# Checks out a remote branch (if it exists) and creates a local branch tracking the remote branch
+git config --global alias.pull-remote '!f() { \
+  if git rev-parse --quiet --verify "$1"; then \
+    echo "Local branch $1 already exists."; \
+  else \
+    git fetch origin "$1" && git checkout -b "$1" --track origin/"$1"; \
+  fi; \
+}; f'
+
+ln -s ~/.dotfiles/.bashrc ~/
