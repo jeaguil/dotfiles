5f58e3f476fc4cc52ff1cfd6cc44f5d7e5c1fb8a
jeaguil <jeaguil@github.com>
2025-01-31 11:14:39 -0800

    move alias funcs to .githelpers

--- install.sh
+++ install.sh
@@ -12,25 +12,8 @@ git config --global push.default current
 git config --global alias.l '!. ~/.dotfiles/.githelpers && pretty_git_log'
 git config --global alias.la '!git l --all'
 
-# git pull-remote <branch-name>
-# Checks out a remote branch (if it exists) and creates a local branch tracking the remote branch
-git config --global alias.pull-remote '!f() { \
-  if git rev-parse --quiet --verify "$1"; then \
-    echo "Local branch $1 already exists."; \
-  else \
-    git fetch origin "$1" && git checkout -b "$1" --track origin/"$1"; \
-  fi; \
-}; f'
+git config --global alias.pull-remote '!. ~/.dotfiles/.githelpers && pull_remote'
 
-# git switch-prefix <prefix>
-# Finds and switches to a local branch that matches the specified prefix
-git config --global alias.switch-prefix '!f() { \
-  branch=$(git branch --list "$1*" | head -n 1 | sed "s/^[* ]*//"); \
-  if [ -z "$branch" ]; then \
-    echo "No branch found matching prefix $1"; \
-  else \
-    git switch "$branch"; \
-  fi; \
-}; f'
+git config --global alias.switch-prefix '!. ~/.dotfiles/.githelpers && switch_prefix'
 
 ln -s ~/.dotfiles/.bashrc ~/
